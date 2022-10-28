26e20ad83f04cfb648faf6cea22951df40dd9ca3
Evgen <evroman4in@gmail.com>
2022-10-28 12:51:07 +0300

    убрал хэдэр у 404 страницы

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,17 @@
 import Header from "./Header";
+import { useRouter } from "next/router";
 
-const Layout = ({ children }) => (
-	<>
-		<Header />
-		{children}
-	</>
-);
+const Layout = ({ children }) => {
+
+	const router = useRouter()
+
+	return (
+		<>
+			{router.pathname !== '/404' && <Header />}
+			{children}
+		</>
+	)
+
+};
 
 export default Layout;
--- pages/404.js
+++ pages/404.js
@@ -1,46 +1,26 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from 'next/link'
-import Heading from "../components/Heading";
 import styles from "../styles/404.module.scss";
 import Image from "next/image";
-
 import img from '../images/starofdeath.png'
 
 const Error = () => {
-	const router = useRouter();
-
-	// useEffect(() => {
-	//   setTimeout(() => {
-	//     router.push('/');
-	//   }, 3000);
-	// }, [router]);
-	// useEffect(() => {
-	// 	const q = document.querySelector('header')
-	// 	q.style.display = 'none'		
-	// })
 
 	return (
 		<div className={styles.wrapper}>
 			<Head>
 				<title>Error</title>
 			</Head>
-			<div className={styles.content}>404
-				{/* <Heading text="404"/> */}
-				{/* <Heading tag="h2" text="Something is going wrong..." /> */}
-				
-
-			</div><div className={styles.image} >
-					<Image src={img} width={690}
-						height={623} alt='404' />
-				</div>
+			<div className={styles.content}>404</div>
+			<div className={styles.image} >
+				<Image src={img} width={690}
+					height={623} alt='404' />
+			</div>
 			<Link href={'/'}>
 				<button className={styles.return}>
 					Return
 				</button>
 			</Link>
-
 		</div>
 	)
 };
